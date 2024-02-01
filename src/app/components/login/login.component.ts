b91f01ed7c78dff9bb2125b47cefc47ae8f314e3
BrunoForcato <bruno_hebeler@hotmail.com>
2024-02-01 20:59:44 -0300

    first-commit

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { LoginModel } from '../../models/loginModel';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.createLoginForm();
+  }
+
+  createLoginForm() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}')]],
+      password: ['', Validators.required]
+    })
+  }
+
+  onSubmit() {
+    console.log(this.loginForm.get('email')?.errors)
+
+    var loginFormValue = this.loginForm.getRawValue() as LoginModel
+    console.log('Your form data : ', loginFormValue);
+  }
+}
